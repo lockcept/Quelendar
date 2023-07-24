ced8cee8ba27bef4a02ef3467564de7f5954d788
lockcept <lockcept@gmail.com>
2023-07-24 18:12:03 +0900

    feat: quest class 정의

---
+++ lib/quest.dart
@@ -0,0 +1,84 @@
+import 'package:nanoid/nanoid.dart';
+
+class Task {
+  int startAt;
+  int? endAt;
+  int value;
+  String? comment;
+
+  Task({
+    required this.startAt,
+    this.endAt,
+    required this.value,
+    this.comment,
+  });
+}
+
+enum AchievementType {
+  minute('minute'),
+  count('count');
+
+  const AchievementType(this.id);
+  final String id;
+}
+
+class Mission {
+  int startAt;
+  int endAt;
+  AchievementType achievementType;
+  int goal;
+  List<Task> taskList;
+
+  Mission({
+    required this.startAt,
+    required this.endAt,
+    required this.achievementType,
+    required this.goal,
+    required this.taskList,
+  });
+}
+
+class Tag {
+  String id;
+  String name;
+
+  Tag({required this.id, required this.name});
+
+  Tag.create({required this.name}) : id = nanoid();
+}
+
+enum RepeatCycle {
+  month('month'),
+  week('week'),
+  days('days'),
+  none('none');
+
+  const RepeatCycle(this.type);
+  final String type;
+}
+
+class Quest {
+  String id;
+  String name;
+  List<Tag> tagList;
+  int startAt;
+  int? endAt;
+  RepeatCycle repeatCycle;
+  List<int> repeatData;
+  AchievementType achievementType;
+  int goal;
+  List<Mission> missionList;
+
+  Quest({
+    required this.id,
+    required this.name,
+    required this.tagList,
+    required this.startAt,
+    this.endAt,
+    required this.repeatCycle,
+    required this.repeatData,
+    required this.achievementType,
+    required this.goal,
+    required this.missionList,
+  });
+}
---
+++ lib/quest_dummy.dart
@@ -0,0 +1,13 @@
+import 'package:quest_tracker/quest.dart';
+
+var quest1 = Quest(
+  id: 'lockcept',
+  name: 'game',
+  tagList: [],
+  startAt: 1600000000,
+  repeatCycle: RepeatCycle.none,
+  repeatData: [],
+  achievementType: AchievementType.minute,
+  goal: 60,
+  missionList: [],
+);
