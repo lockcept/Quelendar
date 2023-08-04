40165e06e45cbf75be41f78f71ba14b3716843fd
lockcept <lockcept@gmail.com>
2023-08-04 22:19:50 +0900

    feat: provider 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quest_tracker/quest_provider.dart';
 import 'package:quest_tracker/quest_tracker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => QuestProvider())],
+    child: const App(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
